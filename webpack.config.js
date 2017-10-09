458c4db70e7a0920fc2eea374704eedf8fd52f37
Mateusz Wilk <matewilk@gmail.com>
2017-10-09 16:52:22 +0100

    Added css-modules composes functionality

--- webpack.config.js
+++ webpack.config.js
@@ -32,11 +32,13 @@ let config = {
       },
       {
         test: /\.css$/,
-        exclude: ["node_modules/react-flexbox-grid"],
-        loaders: [
-          'style-loader',
-          'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]&sourceMap&-minimize'
-        ]
+        loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]&sourceMap&-minimize',
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader!postcss-loader',
+        include: path.join(__dirname, 'node_modules'),
+        exclude: /flexboxgrid/
       },
       {
         test: /\.scss$/,
